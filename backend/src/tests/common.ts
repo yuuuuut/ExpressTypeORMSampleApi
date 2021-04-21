f9f3d3bd14682472b1167bd79665f3b808d9e6e3
Sekiguchi Yuto <yutodazepa@icloud.com>
2021-04-21 10:55:04 +0900

    [Backend] 🍔 add User Api Test

--- backend/src/tests/common.ts
+++ backend/src/tests/common.ts
@@ -5,6 +5,23 @@ import { User } from '../entities'
 
 const ApiReq = request('http://localhost:4000/api')
 
+/**
+ * Test用Userを作成する。
+ */
+async function createTestUser() {
+  const userRepository = getManager().getRepository(User)
+
+  const user = new User()
+  user.id = 'TestUser'
+  user.displayName = 'TestUserName'
+  user.photoURL = 'TestUserPhoto'
+
+  const userData = await userRepository.save(user)
+  if (!userData) throw new Error('Test Failed')
+
+  return userData
+}
+
 async function deleteTestUser(id: string) {
   const userRepository = getManager().getRepository(User)
   const user = await userRepository.findOne(id)
@@ -12,4 +29,4 @@ async function deleteTestUser(id: string) {
   if (user) await userRepository.delete(id)
 }
 
-export { ApiReq, deleteTestUser }
+export { ApiReq, deleteTestUser, createTestUser }
--- backend/src/tests/controllers/apis/user.api.controller.test.ts
+++ backend/src/tests/controllers/apis/user.api.controller.test.ts
@@ -1,13 +1,5 @@
-//import request from 'supertest'
-
 import { TestIResponse, UserCreateApiRes } from '../../../commons/types'
-import { ApiReq, deleteTestUser } from '../../common'
-
-/***************************
- *
- **************************/
-
-//const req = request('http://localhost:4000/api')
+import { ApiReq, createTestUser, deleteTestUser } from '../../common'
 
 /***************************
  *    Main
@@ -28,6 +20,26 @@ describe('User API Controller Test', () => {
       expect(response.status).toEqual(201)
       expect(response.body.data.user).toEqual({ ...user, isAdmin: false })
       expect(response.body.data.isCreate).toEqual(true)
+
+      await deleteTestUser(user.id)
+    })
+
+    it('POST /users Userが存在する場合、Userの作成が行われないこと。', async () => {
+      const user = await createTestUser()
+
+      const newUser = {
+        id: user.id,
+        displayName: user.displayName,
+        photoURL: user.photoURL,
+      }
+
+      const response = (await ApiReq.post('/users').send(
+        newUser
+      )) as TestIResponse<UserCreateApiRes>
+
+      expect(response.status).toEqual(201)
+      expect(response.body.data.user).toEqual({ ...user, isAdmin: false })
+      expect(response.body.data.isCreate).toEqual(false)
 
       await deleteTestUser(user.id)
     })
